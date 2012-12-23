a41c39fc69966103c9b622eb2d9a381dabf5a886
Steven Van Acker <steven@singularity.be>
2012-12-24 00:54:28 +0100

    now possible to execute from commandline

--- BoobiesClassifier.py
+++ BoobiesClassifier.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+import sys
+
 def isBoobiesPicture(url): #{{{
     '''This method should verify if a given URL is a picture that contains boobies.'''
     return False
 #}}}
 
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+	fn = sys.argv[1]
+	print "%s: %s" % (fn, isBoobiesPicture(fn))
