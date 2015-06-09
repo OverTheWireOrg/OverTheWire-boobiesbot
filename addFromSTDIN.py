b2754d81a9c98b46133c94239719e229f7e9208a
Steven Van Acker <steven@singularity.be>
2015-06-09 07:40:29 +0200

    add URLs from stdin into mongo backend

--- addFromSTDIN.py
+++ addFromSTDIN.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python
 
-# SQLite
-import sqlite3
-import os
 import sys
-
-class Adder(object):
-    def db_init(self, fn): #{{{
-	if os.path.exists(fn):
-	    self.db = sqlite3.connect(fn)
-	else:
-	    self.db = sqlite3.connect(fn)
-	    cu = self.db.cursor()
-	    cu.execute("create table boobies (url varchar)")
-	    self.db.commit()
-    #}}}
-    def db_addBoobies(self, url): #{{{
-	cu = self.db.cursor()
-	cu.execute("insert into boobies values(?)", (url,))
-	self.db.commit()
-	return cu.lastrowid
-    #}}}
-
+from boobies.BoobiesDatabaseMongoDB import *
 
 if __name__ == '__main__':
-    x = Adder()
-    x.db_init("boobies.db")
+    x = BoobiesDatabaseMongoDB()
     for l in [s.strip() for s in sys.stdin.readlines()]:
-	x.db_addBoobies(l)
+	x.addBoobies(l)
